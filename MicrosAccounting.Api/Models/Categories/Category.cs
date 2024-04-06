8c14f7af9be52b6c5cbc5057e0b2cd169af74a4f
Azam Amonov <azamamonov555@gmail.com>
2024-04-06 17:22:56 +0500

    DATA: Category

---
+++ MicrosAccounting.Api/Models/Categories/Category.cs
@@ -0,0 +1,10 @@
+using MicrosAccounting.Api.Models.Transactions;
+
+namespace MicrosAccounting.Api.Models.Categories;
+
+public class Category
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public virtual ICollection<Transaction?> Transactions { get; set; }
+}

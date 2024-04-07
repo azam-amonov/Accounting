6a5e8c999c7747cec563cbdbfb743b3d87ca8abd
Azam Amonov <azamamonov555@gmail.com>
2024-04-07 13:47:39 +0500

    INFRA: Add Configurations

--- MicrosAccounting.Api/Program.cs
+++ MicrosAccounting.Api/Program.cs
@@ -1,6 +1,34 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using MicrosAccounting.Api.Brokers.DateTimeBrokers;
+using MicrosAccounting.Api.Brokers.StorageBrokers;
+using MicrosAccounting.Api.Services.Foundations;
+using MicrosAccounting.Api.Services.Foundations.Categories;
+using MicrosAccounting.Api.Services.Foundations.Users;
+using MicrosAccounting.Api.Services.Transactions;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// dbContext
+builder.Services.AddDbContext<StorageBroker>();
+
+// lifecycle
+// brokers
+builder.Services.AddTransient<IStorageBroker, StorageBroker>();
+builder.Services.AddTransient<IDateTimeBroker, DateTimeBroker>();
+
+// services
+builder.Services.AddTransient<IUserService, UserService>();
+builder.Services.AddTransient<ICategoryService, CategoryService>();
+builder.Services.AddTransient<ITransactionService, TransactionService>();
+
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapControllers();
+
 
 app.Run();

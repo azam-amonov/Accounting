cd1518405c823adecbd0266825516574d2093be0
Azam Amonov <azamamonov555@gmail.com>
2024-04-08 09:21:09 +0500

    CODE RUB: Add Type in Category

--- MicrosAccounting.Api/Models/Categories/Category.cs
+++ MicrosAccounting.Api/Models/Categories/Category.cs
@@ -6,5 +6,6 @@ public class Category
 {
     public Guid Id { get; set; }
     public string Name { get; set; }
+    public CategoryAccount Accounting { get; set; }
     public virtual ICollection<Transaction>? Transactions { get; set; }
 }

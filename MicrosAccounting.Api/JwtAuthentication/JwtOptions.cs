4d64264213e9d2fcb0826b3964ee6f60231d0473
Azam Amonov <azamamonov555@gmail.com>
2024-04-10 01:02:45 +0500

    DATA: New Migration

---
+++ MicrosAccounting.Api/JwtAuthentication/JwtOptions.cs
@@ -0,0 +1,7 @@
+namespace MicrosAccounting.Api.JwtAuthentication;
+
+// public record JwtOptions(
+//     string Issuer,
+//     string Audience,
+//     string SecretKey,
+//     int ExpirationSeconds);

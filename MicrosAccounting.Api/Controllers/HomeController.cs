bcd08322477828c0ce851fb7c7ce6c5053cd33c7
Azam Amonov <azamamonov555@gmail.com>
2024-04-11 13:33:35 +0500

    X

--- MicrosAccounting.Api/Controllers/HomeController.cs
+++ MicrosAccounting.Api/Controllers/HomeController.cs
@@ -3,7 +3,6 @@ using System.Security.Claims;
 using System.Text;
 using MicrosAccounting.Api.Services.Foundations.Users;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 

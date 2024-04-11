c4658cd80910a1a09069890e9bc54d6bdd489750
Azam Amonov <azamamonov555@gmail.com>
2024-04-11 20:03:13 +0500

    CODE RUB: Delete unused file

--- MicrosAccounting.Api/JwtAuthentication/JwtOptions.cs
+++
@@ -1,7 +0,0 @@
-namespace MicrosAccounting.Api.JwtAuthentication;
-
-// public record JwtOptions(
-//     string Issuer,
-//     string Audience,
-//     string SecretKey,
-//     int ExpirationSeconds);

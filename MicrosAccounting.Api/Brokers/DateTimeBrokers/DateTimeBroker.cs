08ba36135395471d61bc8882a9cde3b1caae589e
Azam Amonov <azamamonov555@gmail.com>
2024-04-15 10:23:46 +0500

    CODE RUB: Add Hour

--- MicrosAccounting.Api/Brokers/DateTimeBrokers/DateTimeBroker.cs
+++ MicrosAccounting.Api/Brokers/DateTimeBrokers/DateTimeBroker.cs
@@ -3,5 +3,5 @@ namespace MicrosAccounting.Api.Brokers.DateTimeBrokers;
 public class DateTimeBroker : IDateTimeBroker
 {
     public DateTimeOffset GetCurrentDateTimeOffset() =>
-        DateTimeOffset.UtcNow;
+        DateTimeOffset.UtcNow.AddHours(5);
 }
